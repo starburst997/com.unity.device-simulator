bc734aceef739e8e97d712b958fa479c7c0b19a0
Jean-Denis Boivin <jd@boiv.in>
2021-08-04 10:05:51 -0400

    Compile fix for older unity release

--- Editor/Input/LegacyInputBackend.cs
+++ Editor/Input/LegacyInputBackend.cs
@@ -22,22 +22,31 @@ namespace Unity.DeviceSimulator
         {
             if (m_LastEventFrame != m_LastSubmittedEventFrame)
             {
+#if UNITY_2021_2_OR_NEWER
                 Input.SimulateTouch(new Touch()
                 {
                     fingerId = m_NextId,
                     position = m_NextPosition,
                     phase = m_NextPhase
                 });
+#else
+                Input.SimulateTouch(m_NextId, m_NextPosition, m_NextPhase);
+#endif
+
                 m_LastSubmittedEventFrame = m_LastEventFrame;
             }
             else if (m_NextPhase == TouchPhase.Moved || m_NextPhase == TouchPhase.Began)
             {
+#if UNITY_2021_2_OR_NEWER
                 Input.SimulateTouch(new Touch()
                 {
                     fingerId = m_NextId,
                     position = m_NextPosition,
                     phase = TouchPhase.Stationary
                 });
+#else
+                Input.SimulateTouch(m_NextId, m_NextPosition, TouchPhase.Stationary);
+#endif
             }
         }
 
